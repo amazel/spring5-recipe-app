12988812b18ec6dafe4b114a7b5e3934e2e62d6e
amazel <hlezama.neri@gmail.com>
2018-03-08 15:26:43 -0600

    Display Image

--- src/main/java/com/hlezama/demo/controllers/ImageController.java
+++ src/main/java/com/hlezama/demo/controllers/ImageController.java
@@ -1,32 +1,55 @@
 package com.hlezama.demo.controllers;
 
+import com.hlezama.demo.commands.RecipeCommand;
 import com.hlezama.demo.services.ImageService;
 import com.hlezama.demo.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
 @Controller
 public class ImageController {
 
-    ImageService imageService;
-    RecipeService recipeService;
+	ImageService imageService;
+	RecipeService recipeService;
 
-    public ImageController(ImageService imageService, RecipeService recipeService) {
-        this.imageService = imageService;
-        this.recipeService = recipeService;
-    }
+	public ImageController(ImageService imageService, RecipeService recipeService) {
+		this.imageService = imageService;
+		this.recipeService = recipeService;
+	}
 
-    @GetMapping("/recipe/{recipeId}/image")
-    public String getImage(@PathVariable Long recipeId, Model model){
-        model.addAttribute("recipe",recipeService.findCommandById(recipeId));
-        return "/recipe/imageuploadform";
-    }
+	@GetMapping("recipe/{recipeId}/image")
+	public String getImage(@PathVariable Long recipeId, Model model) {
+		model.addAttribute("recipe", recipeService.findCommandById(recipeId));
+		return "/recipe/imageuploadform";
+	}
 
-    @PostMapping("/recipe/{recipeId}/image")
-    public String handleImagePost(@PathVariable Long recipeId, @RequestParam("imagefile") MultipartFile file){
-        imageService.saveImageFile(recipeId,file);
-        return "redirect:/recipe/"+recipeId+"/show";
-    }
+	@PostMapping("recipe/{recipeId}/image")
+	public String handleImagePost(@PathVariable Long recipeId, @RequestParam("imagefile") MultipartFile file) {
+		imageService.saveImageFile(recipeId, file);
+		return "redirect:/recipe/" + recipeId + "/show";
+	}
+
+	@GetMapping("recipe/{recipeId}/recipeimage")
+	public void renderImageFromDB(@PathVariable Long recipeId, HttpServletResponse response) throws IOException {
+		log.debug("ImageController.renderImageFromDB - recipeID: "+recipeId);
+		RecipeCommand recipeCommand = recipeService.findCommandById(recipeId);
+
+		response.setContentType("image/jpeg");
+		InputStream is = new ByteArrayInputStream(ArrayUtils.toPrimitive(recipeCommand.getImage()));
+		IOUtils.copy(is,response.getOutputStream());
+	}
 }
--- src/main/java/com/hlezama/demo/services/ImageServiceImpl.java
+++ src/main/java/com/hlezama/demo/services/ImageServiceImpl.java
@@ -3,6 +3,7 @@ package com.hlezama.demo.services;
 import com.hlezama.demo.domain.Recipe;
 import com.hlezama.demo.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,16 +25,11 @@ public class ImageServiceImpl implements ImageService {
 	@Transactional
 	public void saveImageFile(Long recipeId, MultipartFile file) {
 		try {
-			Byte[] bytesObject = new Byte[file.getBytes().length];
-			int i=0;
-			for (byte byteInFile:file.getBytes()) {
-				bytesObject[i++] = Byte.valueOf(byteInFile);
-			}
 
 			Optional<Recipe> recipeOpt = recipeRepository.findById(recipeId);
 			if (recipeOpt.isPresent()) {
 				Recipe recipe1 = recipeOpt.get();
-				recipe1.setImage(bytesObject);
+				recipe1.setImage(ArrayUtils.toObject(file.getBytes()));
 				recipeRepository.save(recipe1);
 			}
 		} catch (IOException e) {
